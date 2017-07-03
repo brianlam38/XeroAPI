079776e6f5606c2e29fb32a2f52d5ec21009a309
brianlam38 <brian.lam38@gmail.com>
2017-07-03 23:35:40 +1000

    adding contact supplier and non supplier data parsing

--- test.py
+++ test.py
@@ -1,53 +1,86 @@
-2#!/usr/bin/python
+#!/usr/bin/python
 import json
 import auth
 
+####################
+# TESTING INVOICES #
+####################
+'''
 # get specific invoice
-json_single = auth.xero.invoices.get(u'e4a0afbd-aea0-450b-ae23-0ce921e84a77')
-
+inv_single = auth.xero.invoices.get(u'e4a0afbd-aea0-450b-ae23-0ce921e84a77')
 # get all invoices
-json_all = auth.xero.invoices.all()
-
-######################
-# TESTING GET METHOD #
-######################
+inv_all = auth.xero.invoices.all()
 
 # print all data for single invoice
 # TypeError: datetime.datetime(2016, 6, 24, 17, 10, 44, 150000) is not JSON serializable
 # ^using json.dumps() ???
-print "PRINTING SINGLE INVOICE"
-print json_single
-print json_single[0]						
+print "### PRINTING SINGLE INVOICE ###"
+print inv_single
+print inv_single[0]						
 print "\n"
 
 # print pretty data under Addresses array
-print "PRINTING INVOICE ADDRESS INFO"
-print(json.dumps(json_single[0]['Contact']['Addresses'], indent=2))
+print "### PRINTING INVOICE ADDRESS INFO ### "
+print(json.dumps(inv_single[0]['Contact']['Addresses'], indent=2))
 print "\n"
 
 # printing all "AddressType" of Addresses inside single invoice
-print "OTHER"
-for item in json_single[0]['Contact']['Addresses']:
+print "### PRINTING ADDRESSTYPES ###"
+for item in inv_single[0]['Contact']['Addresses']:
 	print "Printing Address Type"
 	print item["AddressType"]
 	print "\n"
 
 # print data from each invoice
-#for item in json_all:
-#	print "PRINTING EACH INVOICE INFO"
-#	print item
-#	print "\n"
+for item in inv_all:
+	print "PRINTING EACH INVOICE INFO"
+	print item
+	print "\n"#
+'''
+####################
+# TESTING CONTACTS #
+####################
 
-######################
-# TESTING PUT METHOD #
-######################
+# get specific contact
+contacts_single = auth.xero.contacts.get(u'565acaa9-e7f3-4fbf-80c3-16b081ddae10')
+# get all contacts
+contacts_all = auth.xero.contacts.all()
+
+# print data from first client in list
+print "### ALL CONTACT DATA ###"
+print contacts_all[0]
+
+# print all non-suppleir client names and their emails
+print "### NON-SUPPLIER CLIENT NAMES AND EMAILS ###"
+for item in contacts_all:
+	if item['IsSupplier'] == 1:
+		print item['Name']
+		if item.has_key('EmailAddress'):
+			print item['EmailAddress'], "\n"
+		else:
+			print "N/A\n"
+
+# print all suppleir client names and their emails
+print "### NON-SUPPLIER CLIENT NAMES AND EMAILS ###"
+for item in contacts_all:
+	if item['IsSupplier'] == 0:
+		print item['Name']
+		if item.has_key('EmailAddress'):
+			print item['EmailAddress'], "\n"
+		else:
+			print "N/A\n"
 
 
 
+####################
+# TESTING PAYMENTS #
+####################
 
-#######################
-# TESTING POST METHOD #
-#######################
+# get specific invoice
+#contact_single = auth.xero.contacts.get(u'e4a0afbd-aea0-450b-ae23-0ce921e84a77')
+# get all invoices
+#contact_all = auth.xero.contact.all()
+
 
 
 
