a6bcd87fa385c3c67f42cf570609fb8989577b18
brianlam38 <brian.lam38@gmail.com>
2017-07-04 00:12:56 +1000

    adding Contact Groups tests and editing invoice tests

--- test.py
+++ test.py
@@ -2,52 +2,28 @@
 import json
 import auth
 
-####################
-# TESTING INVOICES #
-####################
-'''
-# get specific invoice
-inv_single = auth.xero.invoices.get(u'e4a0afbd-aea0-450b-ae23-0ce921e84a77')
 # get all invoices
 inv_all = auth.xero.invoices.all()
 
-# print all data for single invoice
-# TypeError: datetime.datetime(2016, 6, 24, 17, 10, 44, 150000) is not JSON serializable
-# ^using json.dumps() ???
-print "### PRINTING SINGLE INVOICE ###"
-print inv_single
-print inv_single[0]						
-print "\n"
-
-# print pretty data under Addresses array
-print "### PRINTING INVOICE ADDRESS INFO ### "
-print(json.dumps(inv_single[0]['Contact']['Addresses'], indent=2))
-print "\n"
-
-# printing all "AddressType" of Addresses inside single invoice
-print "### PRINTING ADDRESSTYPES ###"
-for item in inv_single[0]['Contact']['Addresses']:
-	print "Printing Address Type"
-	print item["AddressType"]
-	print "\n"
-
-# print data from each invoice
-for item in inv_all:
-	print "PRINTING EACH INVOICE INFO"
-	print item
-	print "\n"#
-'''
-####################
-# TESTING CONTACTS #
-####################
-
-# get specific contact
-contacts_single = auth.xero.contacts.get(u'565acaa9-e7f3-4fbf-80c3-16b081ddae10')
 # get all contacts
 contacts_all = auth.xero.contacts.all()
 
+####################
+# TESTING INVOICES #
+####################
+
+# print invoice contact details
+print "###### PRINTING INVOICE ######"
+for item in inv_all:
+	print item['Contact']
+	print item['Contact']['Phones'], "\n"
+
+#####################################
+# TESTING CONTACTS & CONTACT GROUPS #
+#####################################
+
 # print data from first client in list
-print "### ALL CONTACT DATA ###"
+print "### FIRST CONTACT DATA ###"
 print contacts_all[0]
 
 # print all non-suppleir client names and their emails
@@ -60,7 +36,7 @@ for item in contacts_all:
 		else:
 			print "N/A\n"
 
-# print all suppleir client names and their emails
+# print all supplier client names and their emails
 print "### NON-SUPPLIER CLIENT NAMES AND EMAILS ###"
 for item in contacts_all:
 	if item['IsSupplier'] == 0:
@@ -70,6 +46,14 @@ for item in contacts_all:
 		else:
 			print "N/A\n"
 
+# get all contact groups
+contactGroup_all = auth.xero.contactgroups.all()
+
+# print all contact group details
+print "### CONTACT GROUPS DATA ###"
+for item in contactGroup_all:
+	print "######", item['Name'], "######"
+	print item['Status']
 
 
 ####################
